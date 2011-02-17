b14085206e04c691076783dda95319c8754817d8
Branan Purvine-Riley <branan@gmail.com>
2011-02-17 14:02:56 -0800

    Add helpers to read/write an std::string from a binary stream

---
+++ core/util/streamhelpers.hpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+inline std::string readString(std::iostream& stream) {
+  unsigned int len;
+  char *blob;
+  std::string result;
+
+  stream.read((char*)&len, 4);
+  blob = new char[len+1];
+  blob[len] = '\0';
+  stream.read(blob, len);
+  result = blob;
+  delete[] blob;
+  return result;
+}
+
+inline void writeString(std::iostream& stream, const std::string& str) {
+  unsigned int len = str.size();
+  stream.write((char*)&len, 4);
+  stream.write(str.c_str(), len);
+}
