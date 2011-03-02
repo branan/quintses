a3cee17866bc671a680e16c22c9aae88e17d045e
Branan Purvine-Riley <branan@gmail.com>
2011-03-01 16:53:47 -0800

    Add wrapper class for rapidxml

--- core/util/atomic.hpp
+++ core/util/atomic.hpp
@@ -2,12 +2,17 @@
 #define QNT_CORE_UTIL_ATOMIC_HPP
 
 #if defined(__GNUC__)
-#define fetchAndIncrement(var) __sync_fetch_and_add(&var, 1)
+#define fetchAndIncrementP(var) __sync_fetch_and_add(var, 1)
+#define fetchAndDecrementP(var) __sync_fetch_and_add(var, -1)
 #elif defined(_MSC_VER)
 #include <windows.h>
-#define fetchAndIncrement(var) InterlockedExchangeAdd(&var, 1)
+#define fetchAndIncrementP(var) InterlockedExchangeAdd(&var, 1)
+#define fetchAndDecrementP(var) InterlockedExchangeSd(&var, -1)
 #else
 #error "Sorry, don't know how to do an atomic increment on this compiler"
 #endif
+
+#define fetchAndIncrement(var) fetchAndIncrementP(&var)
+#define fetchAndDecrement(var) fetchAndDecrementP(&var)
 
 #endif // QNT_CORE_UTIL_ATOMIC_HPP
---
+++ core/util/xml.hpp
@@ -0,0 +1,55 @@
+#ifndef QNT_CORE_UTIL_XML_HPP
+#define QNT_CORE_UTIL_XML_HPP
+
+// TODO: adjust RAPIDXML_STATIC_POOL_SIZE and RAPIDXML_DYNAMIC_POOL_SIZE to meet QNT requirements
+#include "3rdparty/rapidxml/rapidxml.hpp"
+#include "atomic.hpp"
+
+#include <string>
+#include <string.h>
+
+class XmlDom {
+public:
+  XmlDom() : m_count(0), m_document(0) {}
+  XmlDom(std::string text) {
+    m_count = new uint32_t;
+    *m_count = 1;
+    m_document = new rapidxml::xml_document<>;
+    // this string will be released when the document is destroyed.
+    char *string = m_document->allocate_string(text.c_str());
+    m_document->parse<0>(string);
+  }
+  ~XmlDom() {
+    if(m_count) {
+      fetchAndDecrementP(m_count);
+      if((*m_count) == 0) {
+        delete m_count;
+        delete m_document;
+      }
+    }
+  }
+  XmlDom(const XmlDom& rhs) : m_count(rhs.m_count), m_document(rhs.m_document) {
+    if(m_count)
+      fetchAndIncrementP(m_count);
+  }
+  XmlDom& operator=(const XmlDom& rhs) {
+    if(m_document) {
+      fetchAndDecrementP(m_count);
+      if((*m_count) == 0) {
+        delete m_count;
+        delete m_document;
+      }
+    }
+    m_count = rhs.m_count;
+    m_document = rhs.m_document;
+    if(m_count)
+      fetchAndIncrementP(m_count);
+    return *this;
+  }
+  rapidxml::xml_document<> *doc();
+private:
+  uint32_t *m_count;
+  rapidxml::xml_document<> *m_document;
+};
+
+#endif // QNT_CORE_UTIL_XML_HPP
