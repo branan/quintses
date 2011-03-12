dc4204e12982d88d950dc825a2549e1d6fb2e58c
Branan Purvine-Riley <branan@gmail.com>
2011-03-12 15:56:48 -0800

    Add a lockless queue implementation, and make it the default

--- core/util/queue.hpp
+++ core/util/queue.hpp
@@ -4,8 +4,10 @@
 #include <deque>
 #include <boost/thread.hpp>
 
+#include "atomic.hpp"
+
 template <typename T>
-class queue {
+class lockedQueue {
 public:
   void push(const T& t) {
     boost::mutex::scoped_lock lock(m_queue_mutex);
@@ -38,6 +40,66 @@ private:
   boost::mutex m_queue_mutex;
   boost::condition_variable m_queue_cond;
 };
+
+template <typename T>
+class locklessQueue {
+  class QueueItem {
+  public:
+    QueueItem() : next(0) {}
+    T value;
+    QueueItem *next;
+  };
+  
+  QueueItem *head;
+
+public:
+  locklessQueue() : head(new QueueItem) {}
+
+  void push(const T& t) {
+    QueueItem *new_head = new QueueItem;
+    new_head->value = t;
+    do {
+      new_head->next = head->next;
+    } while(!compareAndSwapPointer(head->next, new_head->next, new_head));
+  }
+
+  T wait_pop() {
+    QueueItem *item;
+    T result;
+    for(;;) {
+      do {
+        item = head->next;
+        if(item == 0)
+          break;
+      } while(!compareAndSwapPointer(head->next, item, item->next));
+      if(item) {
+        result = item->value;
+        delete item;
+        return result;
+      }
+    }
+  }
+
+  bool try_pop(T& t) {
+    QueueItem *item;
+    do {
+      item = head->next;
+      if(item == 0)
+        return false;
+    } while(!compareAndSwapPointer(head->next, item, item->next));
+    t = item->value;
+    delete item;
+    return true;
+  }
+};
+
+#ifdef QNT_USE_LOCKED_QUEUE
+template <typename T>
+class queue : public lockedQueue<T> {};
+#else
+template <typename T>
+class queue : public locklessQueue<T> {};
+#endif
 
 
 #endif // QNT_CORE_UTIL_QUEUE_HPP
